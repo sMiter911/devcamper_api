4510af54aeb1b913dc457c0810245e41c73be2b4
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-12-12 20:15:57 +0200

    Photo upload, advanced results middleware

--- devcamper_api/controllers/courses.js
+++ devcamper_api/controllers/courses.js
@@ -8,20 +8,19 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: "bootcamp",
-      select: "name description",
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses,
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
-  const courses = await query;
-  res.status(200).json({ success: true, count: courses.length, data: courses });
 });
 
-// @desc    Get a single courses
+// @desc    Get a single course
 // @route   GET /api/v1/course/:id
 // @access  Public
 exports.getCourse = asyncHandler(async (req, res, next) => {
